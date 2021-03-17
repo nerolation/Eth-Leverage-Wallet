bcab2b3b2567ee4facb7813e87ed98997a15042a
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-03-17 08:11:46 +0100

    Non-Formated PoC

--- draft.sol
+++ draft.sol
@@ -4,50 +4,136 @@ contract DssCdpManagerLike {
     mapping (address => uint) public first;     // Owner => First CDPId
 }
 
+
+contract VatLike {
+     struct Ilk {
+            uint256 Art;   // Total Normalised Debt     [wad]
+            uint256 rate;  // Accumulated Rates         [ray]
+            uint256 spot;  // Price with Safety Margin  [ray]
+            uint256 line;  // Debt Ceiling              [rad]
+            uint256 dust;  // Urn Debt Floor            [rad]
+        }
+    mapping (bytes32 => Ilk) public ilks;
+}
 interface ProxyRegistryLike {
     function proxies(address) external view returns (address);
+    function build() external;
 }
 
-contract myCallerContract {
-     address DssProxyActions = 0xd1D24637b9109B7f61459176EdcfF9Be56283a7B;
-     address CPD_MANAGER = 0x1476483dD8C35F25e568113C5f70249D3976ba21;
-     address MCD_JUG = 0xcbB7718c9F39d05aEEDE1c472ca8Bf804b2f1EaD;
-     address ETH_JOIN = 0x775787933e92b709f2a3C70aa87999696e74A9F8;
-     address DAI_JOIN = 0x5AA71a3ae1C0bd6ac27A1f28e1415fFFB6F15B8c;
+interface DaiTokenLike {
+    function approve(address usr, uint wad) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+
+
+interface UniswapV2Router02Like {
+    function WETH() external returns (address);
+}
+
+
+interface UniswapV2Factory{
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
+
+interface UniswapV2Pair {   
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+}
+
+
+contract HelperContract {
+     address DssProxyActions = 0xf2A4966bE1e633Fd3A4b7011Ab543b0AA5050047;
+     address CPD_MANAGER = 0x033b7629CeC52a41712C362868f6cd70aEFc0545;
+     address MCD_JUG = 0x4bc636a867E8B9275aB022C1Ef44d6B24bc65b8d;
+     address ETH_JOIN = 0xa885b27E8754f8238DBedaBd2eae180490C341d7;
+     address DAI_JOIN = 0xA0b569e9E0816A20Ab548D692340cC28aC7Be986;
      bytes32 ilk = 0x4554482d41000000000000000000000000000000000000000000000000000000;
-     address DAI_TOKEN = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;
-     address UNI_Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-     address Wrapped_ETH = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
+     address DAI_TOKEN = 0x31F42841c2db5173425b5223809CF3A38FEde360;
+    
+    // Return minimum draw amount of DAI
+    function getMinDraw() public view returns (uint) {
+        VatLike vat = VatLike(0xFfCFcAA53b61cF5F332b4FBe14033c1Ff5A391eb);
+        (uint Art, uint rate, uint spot , uint line, uint dust) = vat.ilks(ilk);
+        
+        return dust/rate;
+    }
+}
+
+contract myCallerContract is HelperContract{
      
-     DssCdpManagerLike dcml = DssCdpManagerLike(0x1476483dD8C35F25e568113C5f70249D3976ba21);
-     address ProxyRegistry = 0x64A436ae831C1672AE81F674CAb8B6775df3475C;
-     ProxyRegistryLike prl = ProxyRegistryLike(0x64A436ae831C1672AE81F674CAb8B6775df3475C);
+     
+     
+     address UNI_Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+     address private UNI_Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+     address Wrapped_ETH = 0xc778417E063141139Fce010982780140Aa0cD5Ab;   
+     
+     DssCdpManagerLike dcml = DssCdpManagerLike(0x033b7629CeC52a41712C362868f6cd70aEFc0545);
+     ProxyRegistryLike prl = ProxyRegistryLike(0x1b8357eB14Dd29e4D29AC163342Ee838DeEBCC7f);
+     UniswapV2Factory uv2 = UniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+     UniswapV2Router02Like url = UniswapV2Router02Like(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+     DaiTokenLike dtl = DaiTokenLike(DAI_TOKEN);
+     address Wrapped_ETH_Uni =  url.WETH();
+     
      address public proxy;
      uint256 public cdpi;
      
+     function getExchangeRate() public view returns(uint) {
+         (uint a, uint b) = getTokenReserves_uni() ;
+         return a/b;
+         
+     }
      
+     
+     
+     // build DS Proxy for the CallerContract
      function buildProxy() public {
-         (bool success, ) = ProxyRegistry.call(abi.encodeWithSignature("build()"));
+         prl.build();
+         
+         //Safe proxy address to state variable
          proxy = prl.proxies(address(this));
      }
      
-     function openLockETHAndDraw() public payable {
+     // get token reserves from the pool to determine the current exchange rate
+     function getTokenReserves_uni() public view returns (uint, uint) {
+        
+        address pair = uv2.getPair(DAI_TOKEN,Wrapped_ETH_Uni);
+        (uint reserved0, uint reserved1,) = UniswapV2Pair(pair).getReserves();
+        
+        return (reserved0, reserved1);
+        
+    }
+
+    
+     function myContractsDaiBalance() public view returns (uint){
+         return dtl.balanceOf(address(this));
+     }
+     
+     function myContractsWETHBalance() public view returns (uint){
+         return dtl.balanceOf(address(this));
+     }
+     
+     function openLockETHAndDraw(uint value) public payable {         
+         // uint drawAmount = (msg.value / 10**18 ) * getExchangeRate() /2;
          (bool success, ) = proxy.call{
                 value:msg.value
              }(abi.encodeWithSignature(
                  "execute(address,bytes)",
                  DssProxyActions, 
-                 abi.encodeWithSignature("openLockETHAndDraw(address,address,address,address,bytes32,uint256)", CPD_MANAGER, MCD_JUG, ETH_JOIN, DAI_JOIN, ilk, 100000000000000000000)));
+                 abi.encodeWithSignature("openLockETHAndDraw(address,address,address,address,bytes32,uint256)", CPD_MANAGER, MCD_JUG, ETH_JOIN, DAI_JOIN, ilk, value)));
              cdpi = dcml.first(proxy);
     }
     
     function approveUNIRouter(uint value) public  {
-        (bool success, ) = DAI_TOKEN.call(abi.encodeWithSignature("approve(address,uint256)", UNI_Router, 100000000000000000000));
+        (bool success, ) = DAI_TOKEN.call(abi.encodeWithSignature("approve(address,uint256)", UNI_Router, value));
     }
     
-    function  swapThatShit() public  {
-        (bool success, ) = UNI_Router.call(abi.encodeWithSignature("swapTokensForExactETH(uint256,uint256,address[],address,uint256)", 10, 100, 
-        [DAI_TOKEN,Wrapped_ETH], address(this), block.timestamp + 20000));
+    function  swapThatShit(uint value, uint exchangeRate) public  {
+        address[] memory path = new address[](2);
+        path[0] = address(DAI_TOKEN);
+        path[1] = url.WETH();
+        uint amount_in = value;
+        uint amount_out = value/exchangeRate;
+        (bool success, ) = UNI_Router.call(abi.encodeWithSignature("swapExactTokensForETH(uint256,uint256,address[],address,uint256)", amount_in, amount_out, path, address(this), block.timestamp + 20));
     }
     
      function LockETHAndDraw() public payable {
@@ -58,7 +144,12 @@ contract myCallerContract {
                                                                                                                                                              ETH_JOIN, 
                                                                                                                                                             DAI_JOIN, 
                                                                                                                                                              cdpi, 
-                                                                                                                                                             100000000000000000000)));
+                                                                                                                                                             10000000000000000000)));
+    }
+    
+    function payBack(address payable addr) public {
+        addr.transfer(address(this).balance);
+        
     }
     
     receive() external payable{}
